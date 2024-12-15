92673f8fba44eb6c661206085cecd201abf64add
Avirup saha <sahaavirup1998@gmail.com>
2024-12-15 20:41:27 +0530

    make server.js, connect the mongodb and cloudinary

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+import connectCloudinary from "./config/cloudinary.js";
+
+// app configuration
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+connectDB();
+connectCloudinary();
+
+// API endpoint
+app.get('/', (req, res) => {
+    res.send('API connected succesfully!');
+})
+
+app.listen(port, () => {
+    console.log('Server is running on PORT: ' + port); 
+})
+
+// sahaavirup6733
