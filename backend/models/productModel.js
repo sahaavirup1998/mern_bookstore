519170b29d4a3e770830d8a4822cf0543748e7ee
Avirup saha <sahaavirup1998@gmail.com>
2024-12-15 21:17:30 +0530

    create the product and user models

---
+++ backend/models/productModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    date:{
+        type: Number,
+        required: true
+    },
+    popular:{
+        type: Boolean,
+    },
+})
+
+const productModel = mongoose.models.product || mongoose.model('product', productSchema);
+
+export default productModel;
